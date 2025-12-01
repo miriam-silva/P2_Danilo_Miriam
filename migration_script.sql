e35e964b05bedf2612287c24ef29e67fc20145c3
miriam-silva <miriam.correa@fatec.sp.gov.br>
2025-12-01 09:55:17 -0300

    Commit com o script do migration

---
+++ migration_script.sql
@@ -0,0 +1,29 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+CREATE TABLE [Disciplinas] (
+    [Id] int NOT NULL IDENTITY,
+    [Nome] nvarchar(max) NOT NULL,
+    [CargaHoraria] int NOT NULL,
+    [Professor] nvarchar(max) NULL,
+    CONSTRAINT [PK_Disciplinas] PRIMARY KEY ([Id])
+);
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20251201120019_InitialCreate', N'8.0.0');
+GO
+
+COMMIT;
+GO
+
