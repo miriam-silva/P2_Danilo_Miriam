fa8f04bc562af72b02b1839476fc6727c43def3a
miriam-silva <miriam.correa@fatec.sp.gov.br>
2025-12-01 09:47:08 -0300

    Segundo commit da prova

--- Program.cs
+++ Program.cs
@@ -3,21 +3,17 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllers();
 
-// Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// DbContext (IMPORTANTE!)
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
 
 var app = builder.Build();
 
-// Swagger sempre ativo para facilitar testes
 app.UseSwagger();
 app.UseSwaggerUI();
 
